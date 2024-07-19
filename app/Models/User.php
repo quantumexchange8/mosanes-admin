0d377f01b17edbcc45d8921c63c35e8860188caf
Wayne Lai <waynelai06@gmail.com>
2024-07-19 21:13:17 +0800

    feat: table migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,11 +18,7 @@ class User extends Authenticatable implements HasMedia
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = ['id', 'created_at', 'updated_at'];
 
     /**
      * The attributes that should be hidden for serialization.
